64a7f788f32479319a4035459f360c63567bfe0c
Lionel JOUANNAUD <lioneljouannaud@gmail.com>
2026-04-15 20:47:25 +0200

    Use P logo for site icons and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,9 +58,9 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.png", type: "image/png" },
-      { url: "/favicon.ico" },
       { url: "/icon.png", type: "image/png" },
     ],
+    shortcut: [{ url: "/favicon.png", type: "image/png" }],
     apple: [{ url: "/apple-touch-icon.png", type: "image/png" }],
   },
 };
